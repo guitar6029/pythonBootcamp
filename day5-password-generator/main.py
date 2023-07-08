34b1014051a83009c104da716aad854990c12e67
Josh <jsbusiness305@gmail.com>
2023-07-08 18:42:44 -0400

    added day5

---
+++ day5-password-generator/main.py
@@ -0,0 +1,10 @@
+import random
+numberOfCharacters = int(input("How many characters would you like in your password? : "))
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '<', '>', ',', '.', '?', '/']
+password = ''
+array_length = len(alphabet)
+for n in range(0, numberOfCharacters):
+    password += alphabet[random.randint(0, array_length - 1)]            
+print(password)
